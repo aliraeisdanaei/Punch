4e983f95309bfdf26353130f1753db63ea4004cc
Ali Raeisdanaei <mrrookie2@gmail.com>
2020-09-08 18:29:23 -0400

    Implementation of punch in and implementation of initiallization of spreadsheet folder and file

--- punch.sh
+++ punch.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
 crnt_date=`date +%F`
+#checks to see if there has already been a punch in recorded
+lastline=`tail -1 ~/Productivity/work_times.xlsx`
+lastline_day=`echo  $lastline | cut -d "," -f1`
+lastline_month=`echo  $lastline | cut -d "," -f2`
+lastline_year=`echo  $lastline | cut -d "," -f3`
+
+
 
 function punch_in {
-    #checks to see no file for today's information in home dir
-    if [[ -f ~/.$crnt_date ]]
+clear
+    if [ $lastline_day == `date +%d` ] && [ $lastline_month == `date +%m` ] && [ $lastline_year == `date +%Y` ]
     then
-        echo "Already punched in at `cat ~/.$crnt_date | head -1`. Will be redirected shortly. "
+        echo "You have already punched in at `echo  $lastline | cut -d "," -f4`. You will be redirected shortly. "
+        echo -n "Press enter to go back to menu "
+        read x
+        menu
+    else
+        echo `date +%d`,`date +%m`,`date +%Y`,`date +%R` >> ~/Productivity/work_times.xlsx
+        echo "Your have punched in. Remember to punch out when you are done."
+        echo "Punch in time: `date +%R`"
+        exit
+    fi
+}
+
+function punch_out {
+    
+    if ! [[ -f ~/.$crnt_date ]]
+    then
+        echo "You must punch in before punching out."
         echo -n "Press enter to go back to menu "
         read x
         menu
     fi
 
-    clear
-    echo `date +%R` > ~/.$crnt_date
-    echo `date +%R`
-    echo "Your have punched in. Remember to punch out when you are done."
-    echo -n "Press enter to exit "
-    read x
-    exit
-}
+    
 
-function punch_out {
-    echo hi
     
 }
 
@@ -31,7 +45,7 @@ function output_hours_worked {
     
 }
 
-function break {
+function goto_break {
     echo hi
 
 }
@@ -52,10 +66,10 @@ function menu {
             punch_in
             ;;
         2)
-            echo hello
+            punch_out
             ;;
         3)
-            echo inn
+            goto_break
             ;;
         4)
             clear
@@ -68,6 +82,24 @@ function menu {
     esac
 }
 
+function create_fileTemplate {
+    echo "Work Times For $USER" > ~/Productivity/work_times.xlsx
+    echo "File created on `date`" >> ~/Productivity/work_times.xlsx
+    echo >> ~/Productivity/work_times.xlsx
+    echo Day,Month,Year,Entry,Exit,Entry,Exit,Entry,Exit,Entry,Exit,Entry,Exit >> ~/Productivity/work_times.xlsx
+}
+
+#if the directory does not exist create it
+if ! [[ -d ~/Productivity ]]
+then
+
+    mkdir ~/Productivity
+    create_fileTemplate
+elif ! [[ -f ~/Productivity/work_times.xlsx ]]
+then
+    #if the file does not exist create it
+    create_fileTemplate
+fi
 
 menu
 
